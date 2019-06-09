13cc70bec770cfdbd08a7dffd9d5ae271c866e4a
Branimir <bran1mir.bm@gmail.com>
2019-06-09 13:19:57 +0200

    added cpu frequencies to results file

--- Temp_monitor/temp_track.sh
+++ Temp_monitor/temp_track.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-rm temps.csv
+
+rm ~/temps.csv
+GET_TEMP="/opt/vc/bin/vcgencmd measure_temp"
 echo "This is a temperature monitor script. Enter how long do you want to map temperatures."
 read -p "Enter time in minutes: " TOTAL_TIME
-GET_TEMP="/opt/vc/bin/vcgencmd measure_temp"
+
 echo "Temperature tracking began."
 echo "See you in" $TOTAL_TIME "minutes"
 while [ $TOTAL_TIME -ne 0 ]
 do 
-    $GET_TEMP | cut -c 6-9 >> temps.csv
+    $GET_TEMP | cut -c 6-9 | tr -d '\n' >> ~/temps.csv
+    echo -n "," >> ~/temps.csv
+    sudo cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq | tr -d '\n' >> ~/temps.csv
+    echo -n "," >> ~/temps.csv
+    sudo cat /sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_cur_freq | tr -d '\n' >> ~/temps.csv
+    echo -n "," >> ~/temps.csv
+    sudo cat /sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_cur_freq | tr -d '\n' >> ~/temps.csv
+    echo -n "," >> ~/temps.csv
+    sudo cat /sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_cur_freq >> ~/temps.csv
     sleep 60
     ((TOTAL_TIME--))
     echo "Minutes left:" $TOTAL_TIME
