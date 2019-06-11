f7b7fcbfb6c9863094632341f14bfac2238fb3c2
Branimir <bran1mir.bm@gmail.com>
2019-06-11 13:48:27 +0200

    original script

---
+++ Temp_monitor/temp_track.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm ~/temps.csv
+GET_TEMP="/opt/vc/bin/vcgencmd measure_temp"
+echo "This is a temperature monitor script. Enter how long do you want to map temperatures."
+read -p "Enter time in minutes: " TOTAL_TIME
+
+echo "Temperature tracking began."
+echo "See you in" $TOTAL_TIME "minutes"
+while [ $TOTAL_TIME -ne 0 ]
+do 
+    $GET_TEMP | cut -c 6-9 >> ~/temps.csv
+    sleep 60
+    ((TOTAL_TIME--))
+    echo "Minutes left:" $TOTAL_TIME
+done
